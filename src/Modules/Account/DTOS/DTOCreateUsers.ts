7ee04c8f791ed16caaba1d25b120763fb2d03836
Mathues Fellipi <matheus.fellipi@outlook.com>
2021-12-20 12:49:25 -0300

    Deletando

--- src/Modules/Account/DTOS/DTOCreateUsers.ts
+++
@@ -1,8 +0,0 @@
-export interface DTOCreateUsers {
-  id?: string;
-  name: string;
-  email: string;
-  password: string;
-  job_role: string;
-  img_url?: string;
-}
--- src/Modules/Account/Repositories/IUsersRepository.ts
+++
@@ -1,8 +0,0 @@
-import { DTOCreateUsers } from '../DTOS/DTOCreateUsers';
-import { User } from '../Infra/typeorm/Entities/Users';
-
-export interface IUsersRepository {
-  create(data: DTOCreateUsers): Promise<void>;
-  findByEmail(email: string): Promise<User>;
-  findById(id: string): Promise<User>;
-}
