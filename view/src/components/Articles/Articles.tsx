f7a2d43f27635718a0d3c2dc05693b7bd24387d9
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-03-18 22:21:07 -0300

    Criando a conexão com a api

--- view/src/components/Articles/Articles.tsx
+++ view/src/components/Articles/Articles.tsx
@@ -8,88 +8,116 @@ import {
   Stack,
   Divider,
 } from '@chakra-ui/react';
+import { useState } from 'react';
+
+type UsersType = {
+  name: string;
+  job_role: string;
+  avatar: string;
+};
+
+type ArticlesType = {
+  id: string;
+  title: string;
+  themes: string;
+  img_url: string;
+  created_at: string;
+  user: UsersType;
+};
 
 interface ArticlesProps {
-  article;
+  article: ArticlesType[];
 }
 
 export function Articles({ article }: ArticlesProps) {
-  const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
-    imageAlt: 'Rear view of modern home with pool',
-    beds: 3,
-    baths: 2,
-    title: 'Modern home in city center in the heart of historic Los Angeles',
-    formattedPrice: '$1,900.00',
-    reviewCount: 34,
-    rating: 4,
-  };
+  const [listArticle, setlistArticle] = useState<ArticlesType[]>(article);
 
   return (
-    <Box
-      maxW="22rem"
-      borderWidth="1px"
-      bg="white"
-      borderRadius="lg"
-      overflow="hidden"
-    >
-      <Image src={property.imageUrl} alt={property.imageAlt} />
+    <>
+      {listArticle.map((article) => (
+        <Box
+          cursor={'pointer'}
+          key={article.id}
+          maxW="412px"
+          w="412px"
+          margin="5"
+          borderWidth="1px"
+          bg="white"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          <Image
+            src="https://img.freepik.com/vetores-gratis/paisagem-de-marte-planeta-alienigena-fundo-marciano_107791-1781.jpg?t=st=1647618475~exp=1647619075~hmac=aadf3e302b96d6d2e7e5a6873d66f5e4e2611c1fb7bd821da85d60418e6e96c1&w=1380"
+            alt={article.title}
+          />
 
-      <Box padding={'3'}>
-        <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
-          <Text fontSize="lg">{property.title}</Text>
-        </Box>
-        <Flex mt="0.5rem" alignItems={'flex-start'}>
-          <Flex>
-            <Avatar
-              borderColor={'yellow.300'}
-              showBorder
-              size="xs"
-              name="Dan Abrahmov"
-              src="https://bit.ly/dan-abramov"
-            />
-            <Box ml="2">
-              <Text fontSize="8px" fontWeight="bold" flexWrap={'nowrap'}>
-                Segun Adebayo
-                <Badge
-                  fontWeight="normal"
-                  fontStyle="normal"
-                  ml="3"
-                  fontSize="6px"
-                  bgColor={'yellow.300'}
-                  color={'white'}
-                >
-                  Follow
-                </Badge>
+          <Box padding={'3'}>
+            <Box mt="1" as="h4">
+              <Text fontSize="lg" fontWeight="500">
+                {article.title}
               </Text>
-
-              <Text fontSize="8px">UI Engineer</Text>
             </Box>
-          </Flex>
-          <Stack direction="row" h="50px" w="2px" bgColor={'gray.400'} ml={'7'}>
-            <Divider orientation="vertical" bgColor={'gray.400'} />
-          </Stack>
-          <Box ml={'2'} maxW="100px" mr={'1'}>
-            <Text fontSize="8px" color={'gray.400'}>
-              THEME
-            </Text>
-            <Text fontSize="8px" fontWeight="bold" flexWrap={'wrap'}>
-              UX Design, Business, Sales User Research
-            </Text>
+            <Flex mt="0.5rem" alignItems={'flex-start'} justify="center">
+              <Flex w={200} align={'center'}>
+                <Avatar
+                  borderColor={'yellow.300'}
+                  showBorder
+                  w={35}
+                  h={35}
+                  name={article.user.name}
+                  src={article.user.avatar}
+                />
+                <Box ml="2">
+                  <Flex justify="center" align="center">
+                    <Text fontSize="12px" fontWeight="bold">
+                      {article.user.name}
+                    </Text>
+                    <div>
+                      <Badge
+                        fontWeight="normal"
+                        fontStyle="normal"
+                        ml="3"
+                        fontSize="6px"
+                        bgColor={'yellow.300'}
+                        color={'white'}
+                      >
+                        Follow
+                      </Badge>
+                    </div>
+                  </Flex>
+
+                  <Text fontSize="xs">{article.user.job_role}</Text>
+                </Box>
+              </Flex>
+
+              <Flex justify="flex-end">
+                <Stack direction="row" h="50px" w="2px" bgColor={'gray.400'}>
+                  <Divider orientation="vertical" bgColor={'gray.400'} />
+                </Stack>
+                <Box ml={'2'} maxW="130px" w="130px" mr={'1'}>
+                  <Text fontSize="8px" color={'gray.400'}>
+                    THEME
+                  </Text>
+                  <Text fontSize="10px" fontWeight="bold" flexWrap={'wrap'}>
+                    {article.themes}
+                  </Text>
+                </Box>
+                <Stack direction="row" h="50px" w="2px" bgColor={'gray.400'}>
+                  <Divider orientation="vertical" bgColor={'gray.400'} />
+                </Stack>
+                <Box maxW="md" ml={'1'}>
+                  <Text fontSize="8px" color={'gray.400'}>
+                    TEMPS
+                  </Text>
+                  <Text fontSize="8px" fontWeight="bold" flexWrap={'nowrap'}>
+                    4 minutes
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
           </Box>
-          <Stack direction="row" h="50px" w="2px" bgColor={'gray.400'}>
-            <Divider orientation="vertical" bgColor={'gray.400'} />
-          </Stack>
-          <Box maxW="md" ml={'1'}>
-            <Text fontSize="8px" color={'gray.400'}>
-              TEMPS
-            </Text>
-            <Text fontSize="8px" fontWeight="bold" flexWrap={'nowrap'}>
-              4 minutes
-            </Text>
-          </Box>
-        </Flex>
-      </Box>
-    </Box>
+        </Box>
+      ))}
+    </>
   );
 }
