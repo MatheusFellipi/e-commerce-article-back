3b17f05d19bc142e1447ae7957b1aeb572f8ac20
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-03-22 16:01:40 -0300

    rota para pegar por id

--- src/Shared/Infra/Http/Routes/Article.routes.ts
+++ src/Shared/Infra/Http/Routes/Article.routes.ts
@@ -1,18 +1,20 @@
 import multer from 'multer';
 import { CreateArticleController } from '@Modules/Article/UseCases/Articles/CreateArticles/CreateArticleController';
-import { ListArticlesController } from '@Modules/Article/UseCases/Articles/ListCreate/ListArticleController';
+import { ListArticlesController } from '@Modules/Article/UseCases/Articles/ListArticles/ListArticleController';
 
 import { Router } from 'express';
 
 import { ensureAuthenticated } from '../Middlewares/EnsureAuthenticated';
+import { ListByIdArticlesController } from '@Modules/Article/UseCases/Articles/ListByIdArticles/ListByIdArticleController';
 
 const routeArticles = Router();
 
 const createArticleController = new CreateArticleController();
 const listArticlesController = new ListArticlesController();
+const listByIdArticlesController = new ListByIdArticlesController()
 
 routeArticles.get('/', listArticlesController.handle);
-//routeArticles.get('/:id', listArticlesController.handle);
+routeArticles.get('/:id', listByIdArticlesController.handle);
 routeArticles.post('/', ensureAuthenticated, createArticleController.handle);
 
 export { routeArticles };
