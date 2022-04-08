788e70e038f82a077ac08130c11e3e9c0fa1d388
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-04-08 18:48:49 -0300

    AuthProvider

--- view/src/pages/_app.tsx
+++ view/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Header } from '../components/Header/Header';
+import { AuthProvider } from '../services/hook/auth';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider  theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   );
 }
 
