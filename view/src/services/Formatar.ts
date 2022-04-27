22f17bbb8ed79a0b444abe0a13874940b67d5a85
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-04-27 11:46:06 -0300

    Criando a função de formatar datas

---
+++ view/src/services/Formatar.ts
@@ -0,0 +1,15 @@
+export const Formatar = {
+  Money(money: number): string {
+    return money.toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  },
+  Data(dataAtual: Date): string {
+    let data = new Date(dataAtual),
+      dia = data.getDate().toString().padStart(2, '0'),
+      mes = (data.getMonth() + 1).toString().padStart(2, '0'),
+      ano = data.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+  },
+};
