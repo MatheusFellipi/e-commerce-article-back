0a15cadfc70a69e2463b6ae5ffe1b4d2d2f0a617
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-04-29 14:53:37 -0300

    Criando a chamada de api do dash

--- view/src/pages/dashboard/index.tsx
+++ view/src/pages/dashboard/index.tsx
@@ -1,7 +1,25 @@
 import { Box } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
 import NextLink from 'next/link';
+import { parseCookies } from 'nookies';
+import { useState } from 'react';
 
-export default function Dashboard() {
+type DashboardType = {
+  SalesOfArticles: number;
+  ReadArticles: number;
+  CitationsReceived: number;
+  PurchasedArticles: number;
+  listSales: [
+    {
+      title: string;
+      amount: number;
+    }
+  ];
+};
+
+export default function Dashboard(dashborad: DashboardType) {
+  const [datas] = useState<any>(dashborad);
+
   return (
     <Box>
       <h1>Dashboard</h1>
@@ -11,3 +29,31 @@ export default function Dashboard() {
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { ['togdesign:token']: token } = parseCookies(ctx);
+
+  const responce = await fetch('http://localhost:3333/dashborad/', {
+    method: 'GET',
+    headers: {
+      authorization: 'Bearer ' + token,
+    },
+  });
+
+  const data = await responce.json();
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      dashborad: data,
+    },
+  };
+};
