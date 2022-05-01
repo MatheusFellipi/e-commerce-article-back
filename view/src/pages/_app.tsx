cb9b4118294db347f9740ec20dd80d5021ba062d
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-01 14:49:11 -0300

    adicionado highcharts para o dashborad

--- view/src/pages/_app.tsx
+++ view/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Header } from '../components/Header/Header';
 import { AuthProvider } from '../services/hook/auth';
 import { CartProvider } from '../services/hook/useCart';
 import { theme } from '../styles/theme';
+import '../styles/teste.css';
 
 function MyApp({ Component, pageProps }) {
   return (
--- view/src/pages/dashboard/index.tsx
+++ view/src/pages/dashboard/index.tsx
@@ -1,39 +1,169 @@
-import { Box } from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import { GetServerSideProps } from 'next';
-import NextLink from 'next/link';
 import { parseCookies } from 'nookies';
 import { useState } from 'react';
+import { BiBook, BiEdit, BiLineChart } from 'react-icons/bi';
+import { CardDashInfo } from '../../components/CardDashInfo/CardDashInfo';
+import { Formatar } from '../../services/Formatar';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 type DashboardType = {
-  SalesOfArticles: number;
-  ReadArticles: number;
-  CitationsReceived: number;
-  PurchasedArticles: number;
-  listSales: [
-    {
-      title: string;
-      amount: number;
-    }
-  ];
+  dashboard: {
+    SalesOfArticles: number;
+    ReadArticles: number;
+    CitationsReceived: number;
+    PurchasedArticles: number;
+    listSales: [
+      {
+        title: string;
+        amount: number;
+      }
+    ];
+    options: any;
+  };
 };
 
 export default function Dashboard(dashborad: DashboardType) {
-  const [datas] = useState<any>(dashborad);
+  const [data] = useState<any>(dashborad);
+  console.log(data.dashboard.options);
 
   return (
-    <Box>
-      <h1>Dashboard</h1>
-      <NextLink href="/dashboard/published" passHref>
-        published
-      </NextLink>
-    </Box>
+    <Flex justify={'end'} p="10">
+      <Flex
+        flexDirection={'column'}
+        borderRadius={10}
+        justify={'start'}
+        mr={'96px'}
+        w={1080}
+      >
+        <Box
+          mb={'32px'}
+          bgColor={'white'}
+          w={900}
+          borderRadius={10}
+          as={'section'}
+        >
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={data.dashboard.options}
+          />
+        </Box>
+        <Box
+          bgColor={'white'}
+          w={900}
+          h={335}
+          mt={'32px'}
+          as={'section'}
+          borderRadius={10}
+          px={'16px'}
+          py={'24px'}
+        >
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            Your bestsellers
+          </Box>
+
+          <TableContainer
+            fontFamily="Montserrat"
+            fontSize="16px"
+            whiteSpace="normal"
+          >
+            <Table variant="unstyled" size="lg">
+              <Thead>
+                <Th align="left" fontSize="16px" fontWeight={500}>
+                  Article
+                </Th>
+                <Th fontWeight={500} align="left" fontSize="16px">
+                  Publisher
+                </Th>
+                <Th fontWeight={500} align="left" fontSize="16px">
+                  Sales
+                </Th>
+                <Th align="left" fontSize="16px" fontWeight={500}>
+                  Value
+                </Th>
+              </Thead>
+              <Tbody>
+                {data.dashboard.listSales.map((item) => (
+                  <Tr key={item.id}>
+                    <Td maxW={'305px'}>{item.title}</Td>
+                    <Td>Tog.design</Td>
+                    <Td>2</Td>
+                    <Td>{Formatar.Money(item.amount)}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Flex>
+
+      <Box>
+        <CardDashInfo
+          marginRight={'20px'}
+          inf={[
+            {
+              themes: 'Published articles',
+              total: 2,
+            },
+            {
+              themes: 'Sales of articles',
+              total: 2,
+            },
+            {
+              themes: 'Read articles',
+              total: 2,
+            },
+            {
+              themes: 'Purchased articles',
+              total: 2,
+            },
+            {
+              themes: 'Citations received',
+              total: 2,
+            },
+          ]}
+          links={[
+            {
+              href: '/dashboard/purchased',
+              icon: BiBook,
+            },
+            {
+              href: '/dashboard/published',
+              icon: BiEdit,
+            },
+          ]}
+          title={{
+            name: 'dashboard',
+            icon: BiLineChart,
+          }}
+        />
+      </Box>
+    </Flex>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { ['togdesign:token']: token } = parseCookies(ctx);
 
-  const responce = await fetch('http://localhost:3333/dashborad/', {
+  const responce = await fetch('http://localhost:3333/dashboard/', {
     method: 'GET',
     headers: {
       authorization: 'Bearer ' + token,
@@ -53,7 +183,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   return {
     props: {
-      dashborad: data,
+      dashboard: data,
     },
   };
 };
